b483b7cdf10c1f724524b579b1bc9962c6ed59b3
Oscar <oscar.a.chavez.jr@gmail.com>
2021-02-06 13:28:37 -0700

    heroku styling

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 const express = require('express');
 const app = express();
-const path = require('path')
+app.set('view engine', "ejs");
 
-// app.use('/static', express.static('public'));
+app.use(express.static(__dirname + "/public"));
 
-app.use(express.static(__dirname + '/public'));
-
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
+// app.engine('html', require('ejs').renderFile);
+// app.set('view engine', "ejs");
+// app.set('view engine', 'html');
 
 app.get('/', (req, res) => {
-  res.render("Landing.html")
+  res.render("Landing")
 }) 
 
 const PORT = process.env.PORT || 3002;
